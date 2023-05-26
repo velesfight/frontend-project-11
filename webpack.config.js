61f16a2f06fc4075ba270790d2e367d5aa416e3b
Ekaterina Vinnikova <red-white.katya@mail.ru>
2023-05-26 11:52:09 +0300

    import

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
 
 const isProduction = process.env.NODE_ENV == "production";
 
